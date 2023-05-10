c77ed80bb7239416abe2c58009328356e577c97e
Adri-12-svg <adrij40b@gmail.com>
2023-05-11 03:10:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+  document.getElementById('mouse-over').addEventListener('mouse-over',()=>{
+    <FontAwesomeIcon icon="fa-solid fa-messages" beat />
+  })
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+  
+  
